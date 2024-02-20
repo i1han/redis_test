fcd035adfb597157216489c5ffc47229c32a7c50
i1han <B@GROWIN-IH2>
2024-02-20 15:53:22 +0900

    edit 2024-02-20 i1han

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,11 +1,13 @@
 # FlaskApp/app/routes/post.py
 from flask import Blueprint, request, render_template, redirect, url_for, session, flash
-from datetime import datetime
-from app import mysql, redis_cluster  # app 모듈에서 직접 mysql 객체를 가져옵니다. 이 객체를 통해 데이터베이스 작업을 수행합니다.
+from app import mysql, redis_cluster
 import MySQLdb.cursors
+import json
+from datetime import datetime
 
-# 포스트 관련 함수를 위한 블루프린트 생성
 post = Blueprint('post', __name__)
+serialized_posts = []
+
 
 @post.route('/posts', methods=['GET', 'POST'])
 def posts():
@@ -18,23 +20,50 @@ def posts():
     user_id = session.get('user_id')
     session_data = redis_cluster.hgetall(f'user:{user_id}:session')
 
-    # MySQL 데이터베이스 연결에 대한 커서를 생성합니다.
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    # 캐시된 게시물을 불러옵니다.
+    cached_posts = redis_cluster.get('cached_posts')
     
-    try:
-        # 모든 게시물을 조회합니다.
-        cursor.execute('SELECT * FROM post')
-        posts = cursor.fetchall()  # 검색 결과를 가져옵니다.
-    except Exception as e:
-        # 데이터베이스 쿼리 중 오류가 발생한 경우 오류 메시지를 표시합니다.
-        flash(f'오류가 발생했습니다: {e}', 'error')
-        return render_template('posts.html', posts=[], session_data=session_data)
+    serialized_posts = []
     
+    if cached_posts:
+        # 캐시된 게시물이 있는 경우 디코딩하여 사용합니다.
+        posts = json.loads(cached_posts)
+        serialized_posts = posts  # 값 할당
+
+    else:
+        try:
+            # MySQL 데이터베이스 연결에 대한 커서를 생성합니다.
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        
+            # 모든 게시물을 조회합니다.
+            cursor.execute('SELECT * FROM post ORDER BY created_at DESC')
+            posts = cursor.fetchall()  # 검색 결과를 가져옵니다.
+            
+            # 각 게시물을 딕셔너리로 변환하여 직렬화합니다.
+
+            for post in posts:
+                serialized_post = {
+                    'id': post['id'],
+                    'title': post['title'],
+                    'content': post['content'],
+                    'created_at': post['created_at'].strftime('%Y-%m-%d %H:%M:%S'),
+                    'user_id': post['user_id']
+                }
+                serialized_posts.append(serialized_post)
+            
+            # 캐시된 게시물을 Redis에 저장합니다.
+            redis_cluster.set('cached_posts', json.dumps(serialized_posts))
+        except Exception as e:
+            # 데이터베이스 쿼리 중 오류가 발생한 경우 오류 메시지를 표시합니다.
+            flash(f'오류가 발생했습니다: {e}', 'error')
+            return render_template('posts.html', posts=[], session_data=session_data)
+
     # 세션에서 사용자 이름을 가져와 템플릿에 전달합니다.
     session_data = {'username': session.get('username')}
     
-    # 게시물 목록 페이지를 렌더링합니다.
-    return render_template('posts.html', posts=posts, session_data=session_data)
+    return render_template('posts.html', posts=serialized_posts, session_data=session_data)
+
+
 
 @post.route('/createposts', methods=['GET', 'POST'])
 def createposts():
@@ -48,7 +77,7 @@ def createposts():
         title = request.form['title']  # 폼 데이터에서 게시물 제목을 추출합니다.
         content = request.form['content']  # 폼 데이터에서 게시물 내용을 추출합니다.
         user_id = session.get('user_id')  # 세션에서 사용자 ID를 추출합니다.
-        created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # 현재 시간을 문자열로 형식화합니다.
+        created_at = datetime.now()  # 현재 시간을 가져옵니다.
 
         try:
             # MySQL 데이터베이스에 새로운 게시물 데이터를 삽입합니다.
@@ -56,6 +85,21 @@ def createposts():
             cursor.execute('INSERT INTO post (title, content, created_at, user_id) VALUES (%s, %s, %s, %s)',
                            (title, content, created_at, user_id))
             mysql.connection.commit()  # 변경 사항을 커밋합니다.
+            
+            # 게시물이 성공적으로 추가되었으므로 캐시된 데이터를 업데이트합니다.
+            cursor.execute('SELECT * FROM post ORDER BY created_at DESC')
+            posts_tuple = cursor.fetchall()  # 새로운 게시물이 추가된 목록을 가져옵니다.
+            
+            # 각 게시물을 딕셔너리로 변환하여 직렬화합니다.
+            serialized_posts = []
+            for post_tuple in posts_tuple:
+                post = {'id': post_tuple[0], 'title': post_tuple[1], 'content': post_tuple[2], 'created_at': post_tuple[3], 'user_id': post_tuple[4]}
+                post['created_at'] = post['created_at'].strftime('%Y-%m-%d %H:%M:%S')
+                serialized_posts.append(post)
+            
+            # 캐시된 게시물을 Redis에 업데이트합니다.
+            redis_cluster.set('cached_posts', json.dumps(serialized_posts))
+            
             flash('게시물이 성공적으로 작성되었습니다!', 'success')  # 성공 메시지를 표시합니다.
         except Exception as e:
             flash(f'오류가 발생했습니다: {e}', 'error')
